ca06b509074afdf0402b1786d57da354e878634c
Bilal0335 <bilalhussain42201.com>
2025-03-05 11:03:20 +0500

    update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
