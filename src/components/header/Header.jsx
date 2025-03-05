d41a0c941feeeb63814f4aa52a5e33a0a33c554f
Bilal0335 <bilalhussain42201.com>
2025-03-05 12:01:42 +0500

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,24 @@
-import CTA from './CTA';
-import './header.css'
-import ME from "../../assets/me.png"
-import HeaderSocials from './HeaderSocials';
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
-  <header>
-    <div className="container header-container">
-      <h5>Hello's I'm</h5>
-      <h1>Bilal</h1>
-      <h5 className='text-light'>
-        FullStack Developer
-      </h5>
-      <CTA/>
-      <HeaderSocials/>
-      <div className="me">
-        <img src={ME} alt="" />
+    <header>
+      <div className="container header-container">
+        <h5>Hello's I'm</h5>
+        <h1>i am fine</h1>
+        <h5 className="text-light">FullStack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scrool_down">
+          Scroll Down
+        </a>
       </div>
-      <a href="#contact" className='scrool_down'>Scroll Down</a>
-    </div>
-  </header>
+    </header>
   );
 };
 
